f9f990a13a1784a4ae58c6138e9ec450fcacb1a3
ddanny16 <dammydan16@gmail.com>
2022-05-16 15:11:26 +0200

    window.alert(5+6);

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function myFunction(){
     document.getElementById('demo').innerHTML =5 + 6;
+    // document.getElementById('demo').innerHTML ="";
     
 }
+window.alert(5+6);
 
 document.getElementById("demos").addEventListener('click', myFunction)
